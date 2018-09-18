fda389fef4c256ebbca6908bddcfe91204f5a446
xianglijun <275923192@qq.com>
2018-09-18 18:18:50 +0800

    up

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
+    //player
     public float playermovespeed;
     public GameObject playermovepoint;
     private Plane playerplane;
     private GameObject pmp;
     private bool move = false;
+    Animation anim;
+    
+    //
+    private bool canattack = false;
+    private GameObject m_enemy;
     // Use this for initialization
     void Start () {
 
         playerplane = new Plane(Vector3.up, this.transform.position);
+        anim = GetComponent<Animation>();
     }
 
     // Update is called once per frame
@@ -20,38 +27,40 @@ public class player : MonoBehaviour {
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         float hitdistance = 0f;
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)|| Input.GetMouseButton(0))
         {
             if (playerplane.Raycast(ray, out hitdistance))
             {
                 Vector3 moveposition = ray.GetPoint(hitdistance);
-                if (pmp == null)
-                {
-                    pmp = Instantiate(playermovepoint, moveposition, Quaternion.identity);
-                }
-                else
-                {
-
+                if (pmp != null)
                     Destroy(pmp);
-                    pmp = Instantiate(playermovepoint, moveposition, Quaternion.identity);
-
-                }
+                pmp = Instantiate(playermovepoint, moveposition, Quaternion.identity);
                 move = true;
             }
+           
         }
-        if(move)
-           moving();
+         
+        if (move)
+            moving();
+        else
+        {
+            if (canattack)
+                attacking();
+            else
+                anim.CrossFade("idle");
+        }
+          
     }
     void moving()
     {
         transform.position = Vector3.MoveTowards(transform.position, pmp.transform.position, playermovespeed);
         transform.LookAt(pmp.transform);
-        //if (transform.position == pmp.transform.position)
-        //{
-        //    if(pmp!=null)
-        //        Destroy(pmp);
-        //    move = false;
-        //}
+        anim.CrossFade("walk");
+    }
+    void attacking()
+    {
+        transform.LookAt(m_enemy.transform);
+        anim.CrossFade("attack");
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -59,6 +68,18 @@ public class player : MonoBehaviour {
         {
             Destroy(other.gameObject);
             move = false;
+        }
+        if(other.tag == "Enemy")
+        {
+            canattack = true;
+            m_enemy = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            canattack = false;
         }
     }
 }
