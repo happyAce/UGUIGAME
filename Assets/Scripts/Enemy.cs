838b03988d2ef1c8448dd6ea5117f8efaa8fbe18
xianglijun <275923192@qq.com>
2018-09-20 01:29:36 +0800

    up

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,6 +15,18 @@ public class Enemy : MonoBehaviour {
     void Update () {
         anim.CrossFade("idle");
 	}
+    
+    private void OnMouseOver()
+    {
+        Shader sd = Shader.Find("Custom/OutLine2");
+        this.gameObject.GetComponentInChildren<Renderer>().material.shader = sd;
+    }
+    private void OnMouseExit()
+    {
+        Shader sd = Shader.Find("Legacy Shaders/Diffuse");
+        this.gameObject.GetComponentInChildren<Renderer>().material.shader = sd;
+        
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "Player")
